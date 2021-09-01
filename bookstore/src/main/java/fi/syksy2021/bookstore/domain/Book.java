80c9e18cd8d73b0453dc49d45c6dc1d60504b230
Jenni <jentsu.karoliina@hotmail.com>
2021-09-01 08:32:43 +0300

    First commit

---
+++ bookstore/src/main/java/fi/syksy2021/bookstore/domain/Book.java
@@ -0,0 +1,48 @@
+package fi.syksy2021.bookstore.domain;
+
+public class Book {
+
+	private String title, author,isbn;
+	private int year, price;
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public Book(String title, String author, String isbn, int year, int price) {
+		super();
+		this.title = title;
+		this.author = author;
+		this.isbn = isbn;
+		this.year = year;
+		this.price = price;
+	}
+	
+	
+}
---
+++ bookstore/src/main/java/fi/syksy2021/bookstore/web/BookController.java
@@ -0,0 +1,11 @@
+package fi.syksy2021.bookstore.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookController {
+
+	@RequestMapping(value="/index")
+	
+}
